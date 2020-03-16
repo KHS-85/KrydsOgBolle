fbb0a3a7abded98887cb586f243aa69e76bcbe9c
KHS-85 <60042770+KHS-85@users.noreply.github.com>
2020-03-16 17:31:19 +0100

    Kryds og Bolle

---
+++ precache-manifest.a3b87b75dacf359d70a8c066f4b98cdd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a7a41e91369415284bceb0379b1e0df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "248abf5a4239c690f6f1",
+    "url": "/static/css/main.32b86b30.chunk.css"
+  },
+  {
+    "revision": "c7e9992f9770081871e5",
+    "url": "/static/js/2.c77d660b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.c77d660b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "248abf5a4239c690f6f1",
+    "url": "/static/js/main.6d8c3483.chunk.js"
+  },
+  {
+    "revision": "0ebe83331daa3c09a551",
+    "url": "/static/js/runtime-main.6a95d14f.js"
+  }
+]);
